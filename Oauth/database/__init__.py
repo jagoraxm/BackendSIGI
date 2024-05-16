c94419f2bf95c1afef1d423792c92d16281dd27f
jagoraxm <jagoraxr.moucha@gmail.com>
2024-05-16 14:20:51 -0600

    Feature: Carga Inicial

--- Oauth/database/__init__.py
+++
@@ -1,3 +0,0 @@
-app.config.from_mapping(
-        DATABASE_PORT=os.environ.get('FLASK_DATABASE_PORT')
-)
--- Oauth/database/mysql/myDatabase.py
+++ Oauth/database/mysql/myDatabase.py
@@ -3,4 +3,4 @@ import pymysql
 
 def obtener_conexion():
     #return pymysql.connect(host='127.0.0.1', user='root', password='root', db='SIGI', port=8889)
-    return pymysql.connect(host='roundhouse.proxy.rlwy.net', user='root', password='FfNmAOLVGPkIpORMPZbnYROsWhQPsGNF', db='railway', port=current_app.config["DATABASE_PORT"])
+    return pymysql.connect(host='roundhouse.proxy.rlwy.net', user='root', password='FfNmAOLVGPkIpORMPZbnYROsWhQPsGNF', db='railway', port=38158)
