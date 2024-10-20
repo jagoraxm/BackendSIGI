f8184c710c93f01ca3d80386b5254438984c1bd9
jagoraxr <jagoraxr.moucha@gmail.com>
2024-10-20 03:34:03 -0600

    feature: add function sendMail and Validate Register

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
-from core import app
+from core import create_app
+
+app = create_app()
 
 if __name__ == "__main__":
     is_debug = os.getenv('FLASK_ENV') == 'development'
---
+++ config.py
@@ -0,0 +1,10 @@
+# config.py
+
+class Config:
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USERNAME = 'ipn.sigi@gmail.com'
+    MAIL_PASSWORD = 'uhvg wpnj zgpm otem'
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    MAIL_DEFAULT_SENDER = 'ipn.sigi@gmail.com'
